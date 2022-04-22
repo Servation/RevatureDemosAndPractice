c087d011e5fe188eb5013429f5b23f519a761675
Jeffrey Saelee <gwfds123@gmail.com>
2022-04-22 08:00:37 -0700

    Day 8 Demo1

---
+++ Day8/Demo1.java
@@ -0,0 +1,27 @@
+package com.revature.Day8;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        List<String> mList =  new ArrayList<>();
+        mList.add("Mark");
+        mList.add("Paul");
+        mList.add("John");
+
+        System.out.println(mList);;
+
+        // iterator
+        Iterator iterator = mList.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println("************************");
+        // forEach
+        for(String name : mList){
+            System.out.println(name);
+        }
+    }
+}
